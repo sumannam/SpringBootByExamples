048eee65e87be95d6eb38383f403d656aee4ebfb
superboyking <0311pjs@naver.com>
2019-05-09 12:23:16 +0900

    05. 09

--- Problem Bank/Exam0/13_박준석/Helocontroller.java
+++ Problem Bank/Exam0/13_박준석/Helocontroller.java
@@ -1,35 +1,53 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
-public class Helocontroller {
-	@RequestMapping(value= "/", method=RequestMethod.GET)
+@RestController
+public class HeloController 
+{
+	/**
+	 * *
+	 * @param 
+	 * @return mav 		setviewName으로 받은 index html
+	 * 
+	 * @remark get방식으로 사용자로부터 보여지는 프론트엔드 페이지를 반환해 처리 
+	 */
+	@RequestMapping(value="/", method=RequestMethod.GET)
 	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
-		mav.addObject("msg","폼을 전송해주세요.");
 		return mav;
 	}
 	
-	@RequestMapping(value= "/", method=RequestMethod.POST)
-	public ModelAndView send(
-			@RequestParam(value="check1", required=false)boolean check1,
-			@RequestParam(value="radio1", required=false)String radio1,
-			@RequestParam(value="select1", required=false)String select1,
-			@RequestParam(value="select2", required=false)String[] select2,
-			ModelAndView mav) {
+	/**
+	 * 
+	 * @param str1		html로 부터 받은 num1 값
+	 * @param str2		html로 부터 받은 num2 값
+	 * @param mav		ModelAndView 클래스의 인스턴스
+	 * @return			인스턴스 반환
+	 * 
+	 * @remark		매개변수로 받은 문자열 num1과 num2를 정수로 변환해 계산 후 처리
+	 */
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView send(@RequestParam(value="num1",required=false)String str1
+							 ,@RequestParam(value="num2",required=false)String str2
+							 , ModelAndView mav) {
 		
-		String res = "";
+		String res="";
+		int num1 = Integer.parseInt(str1);		//문자열 정수로 변환
+		int num2 = Integer.parseInt(str2);		//문자열 정수로 변환
 		try {
-			res = "check:" + check1 +
-				" radio: " + radio1 +
-				" select: " + select1 +
-				"\nselect2:";
+			int result = num1 + num2;
+			res = String.valueOf(result);		//결과값을 문자열로 변환
+			
 		} catch(NullPointerException e) {
 			res += "null";
 		}
