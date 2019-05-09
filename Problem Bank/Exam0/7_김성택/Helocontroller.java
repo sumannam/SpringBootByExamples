8712d78d71fe426cb694662291cd553c2c3bc6a4
kstkst <kimseongtaek1@gmail.com>
2019-05-09 15:12:29 +0900

    김성택

--- Problem Bank/Exam0/7_김성택/Helocontroller.java
+++ Problem Bank/Exam0/7_김성택/Helocontroller.java
@@ -9,54 +9,22 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class Helocontroller {
 
-		/**
-		 *
-		 *	@fn			public ModelAndView index(ModelAndView mav)
-		 *	@brief		초기 페이지 설정
-		 *
-		 *	@author		김성택
-		 *	@date		2019-05-09
-		 *
-		 *	@param		mav	ModelAndView
-		 *
-		 *
-		 * 	@remark		웹 페이지를 불러오기 위한 초기페이지 설정[2019-05-09;김성택] 
-		 *				
-		 *
-		 */
+
 	
-        @RequestMapping(value="/{msg}", method=RequestMethod.GET)
+        @RequestMapping(value="/", method=RequestMethod.GET)
         public ModelAndView index(ModelAndView mav) {
                 mav.setViewName("index");
-                mav.addObject("msg","변환할환율");
+                mav.addObject("msg","환율변경");
                 return mav;
         }
 
-		/**
-		 *
-		 *	@fn			public ModelAndView send(@RequestParam("num1")String str1,
-		 *										 @RequestParam("num2")String str2, ModelAndView mav)
-		 *
-		 *	@brief		폼에 num1에 있는 값을 str1에 문자열로 받아옴
-		 *
-		 *	@author		김성택
-		 *	@date		2019-05-09
-		 *
-		 *	@param		mav	ModelAndView
-		 *
-		 *
-		 * 	@remark		 결과를 출력하기 위한 dresult 선언 [2019-05-09; 김성택] \n
-		 *				
-		 *
-		 */
+
         
         @RequestMapping(value="/", method=RequestMethod.POST)
-        public ModelAndView send(@RequestParam("num1")String str1,
-        						 
-                        ModelAndView mav) {
+        public ModelAndView send(@RequestParam("num1")String str1,ModelAndView mav) {
         	int result = Integer.parseInt(str1);
-        	int dresult = result/1000;
-                mav.addObject("msg","result = " + dresult);
+        	int result2 = result*1172;
+                mav.addObject("msg","result = " + result2);
                 mav.setViewName("index");
                 return mav;
         }
