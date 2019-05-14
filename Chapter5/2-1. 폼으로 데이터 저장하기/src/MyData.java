67dca00e574ed1a740085fa7f68bb06491597dae
jeiu.smnam <jeiu.smnam@gmail.com>
2019-05-14 15:27:06 +0900

    Add project 2-1, 2-2, 2-3 in Chapter 5

---
+++ Chapter5/2-1. 폼으로 데이터 저장하기/src/MyData.java
@@ -0,0 +1,67 @@
+package com.smnam.springboot;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "mydata")
+public class MyData {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
+	private long id;
+
+	@Column(length = 50, nullable = false)
+	private String name;
+	@Column(length = 200, nullable = true)
+	private String mail;
+	@Column(nullable = true)
+	private Integer age;
+
+	@Column(nullable = true)
+	private String memo;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+}
---
+++ Chapter5/2-2. 데이터 초기화 처리/src/HeloController.java
@@ -0,0 +1,64 @@
+package com.smnam.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import com.smnam.springboot.repositories.MyDataRepository;
+import javax.annotation.PostConstruct;
+
+@Controller
+public class HeloController 
+{
+	// import javax.annotation.PostConstruct; 코드 상단에 추가
+	@PostConstruct
+	public void init(){
+        MyData d1 = new MyData();
+        d1.setName("kim");
+        d1.setAge(123);
+        d1.setMail("kim@gilbut.co.kr");
+        d1.setMemo("this is my data!");
+        repository.saveAndFlush(d1);
+        MyData d2 = new MyData();
+        d2.setName("lee");
+        d2.setAge(15);
+        d2.setMail("lee@flower");
+        d2.setMemo("my girl friend.");
+        repository.saveAndFlush(d2);
+        MyData d3 = new MyData();
+        d3.setName("choi");
+        d3.setAge(37);
+        d3.setMail("choi@happy");
+        d3.setMemo("my work friend...");
+        repository.saveAndFlush(d3);
+	}
+
+    
+    @Autowired
+    MyDataRepository repository;
+    
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView index(
+    		@ModelAttribute("formModel") MyData mydata
+    		, ModelAndView mav) 
+    {
+    	mav.setViewName("index");
+    	mav.addObject("msg","this is sample content.");
+    	Iterable<MyData> list = repository.findAll();
+    	mav.addObject("datalist",list);
+    	return mav;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @Transactional(readOnly=false)
+    public ModelAndView form(
+    		@ModelAttribute("formModel") MyData mydata
+    		, ModelAndView mav)
+    {
+    	repository.saveAndFlush(mydata);
+    	return new ModelAndView("redirect:/");
+    }
+}
