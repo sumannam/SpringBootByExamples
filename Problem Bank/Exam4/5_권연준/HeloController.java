dfb270c6b27519ff8f304d850b8c4c023e68fb0a
kyunjun <kyunjun98@naver.com>
2019-05-16 11:47:47 +0900

    @param Modified

--- Problem Bank/Exam4/5_권연준/HeloController.java
+++ Problem Bank/Exam4/5_권연준/HeloController.java
@@ -34,16 +34,18 @@ public class HeloController {
 
 	/**
 	 * 	@fn	public ModelAndView send
-	 *	@brief	input box에서 weight(몸무게)와 height(키)의 입력값을 받아 몸무게/(키*키)
+	 *	@brief		input box에서 weight(몸무게)와 height(키)의 입력값을 받아 몸무게/(키*키)
 	 *	@details
 	 *
-	 *	@author	권연준
-	 *	@date	2019-05-16
+	 *	@author		권연준
+	 *	@date		2019-05-16
 	 *
-	 *	@param	resPlus = 덧셈
-	 *  @param	res = 체질량지수(BMI)
+	 *	@param  	name = 이름
+	 *	@param  	height = 키
+	 *	@param  	weight = 몸무게
+	 *  @param		res = 체질량지수(BMI)
 	 *  
-	 *	@remark 자신의 체중질량이 정상인지 알기 위해서 [2019-05-16; 권연준]
+	 *	@remark 	자신의 체중질량이 정상인지 알기 위해서 [2019-05-16; 권연준]
 	 */
 	@RequestMapping(value="/",method=RequestMethod.POST)
 	public ModelAndView send( @RequestParam(value="name",required=false)String name,
