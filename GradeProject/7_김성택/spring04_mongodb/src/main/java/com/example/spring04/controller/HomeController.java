d3de128ccf320db89d931f2c2b87de171f38c76a
kstkst <kimseongtaek1@gmail.com>
2019-06-17 00:11:17 +0900

    기본주소설정

--- GradeProject/7_김성택/spring04_mongodb/src/main/java/com/example/spring04/controller/HomeController.java
+++ GradeProject/7_김성택/spring04_mongodb/src/main/java/com/example/spring04/controller/HomeController.java
@@ -3,12 +3,27 @@ package com.example.spring04.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller //컨트롤러 빈으로 등록
+		/**
+		*
+		* @fn 		public String home()
+		* 
+		* @brief 	기본주소설정 
+		*
+		* @author 	김성택
+		* @date 	2019-06-16
+		*
+		* @param 	"home"
+		*
+		* @remark 	home이라는 파일을 기본주소로 설정		[2019-06-16; 김성택]
+		* 		   	
+		**/
+
+@Controller
 public class HomeController {
-	// http://localhost
+
 	@RequestMapping("/")
 	public String home() {
-		// /WEB-INF/view/home.jsp
-		return "home";  // home.jsp로 포워딩
+
+		return "home";
 	}
 }
