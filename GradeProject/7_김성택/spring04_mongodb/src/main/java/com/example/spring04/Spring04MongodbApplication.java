d11840d12a4997a4c861f1b06784fdcc25554552
kstkst <kimseongtaek1@gmail.com>
2019-06-16 23:47:45 +0900

    몽고디비 설정

--- GradeProject/7_김성택/spring04_mongodb/src/main/java/com/example/spring04/Spring04MongodbApplication.java
+++ GradeProject/7_김성택/spring04_mongodb/src/main/java/com/example/spring04/Spring04MongodbApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.spring04;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo210Application {
+public class Spring04MongodbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo210Application.class, args);
+		SpringApplication.run(Spring04MongodbApplication.class, args);
 	}
 
 }
